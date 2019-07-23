3f68928adac0118336a5a043a2920f0d48fc0ece
eric ng <naclericny@gmail.com>
2019-07-22 22:58:30 -0400

    added littlelegends.js

---
+++ littlelegends.js
@@ -0,0 +1,34 @@
+const littlelegends = {
+  egg_series_1: {
+    legendary: {
+      skins: [
+        'Silverwing_Lost',
+        'Furyhorn_Sugarcone',
+        'Hauntling_Shadowmask'
+      ]
+    },
+    epic: {
+      skins: [
+        'Silverwing_Tropical',
+        'Silverwing_Snow_Cherry',
+        'Furyhorn_Void-marked',
+        'Furyhorn_Willowbark',
+        'Hauntling_Dark_Matter',
+        'Hauntling_Lunar_Claw'
+      ]
+    },
+    rare: {
+      skins: [
+        'Silverwing_Demacian',
+        'Silverwing_Dawnglow',
+        'Silverwing_Rosebloom',
+        'Furyhorn_Molten',
+        'Furyhorn_Lionheard',
+        'Furyhorn_Tundra',
+        'Hauntling_Shadow_Isles',
+        'Hauntling_Fireborn',
+        'Hauntling_Toxic'
+      ]
+    }
+  }
+}
